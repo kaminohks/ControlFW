390bed7aea10f178f004466436b3d2e66a4653ce
Arthur Kamino <arthur.dartora.tds2023@gmail.com>
2025-07-01 10:33:37 -0300

    Mensagens de erro.

---
+++ mensagens.php
@@ -0,0 +1,6 @@
+<?php
+
+$mensagens = ["Erro 0: Não foi possível criar diretórios", 
+"Erro 1: Falha ao acessar o banco de dados", 
+"Erro 2: Não foi possível mostrar as tabelas do banco", 
+"Erro 3: Não foi possível mostrar as colunas da tabela"];
